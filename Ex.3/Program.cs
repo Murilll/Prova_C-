9ecfdf7aa07e20783d55a947469070db30f56faa
Murilll <murilosertorio25@gmail.com>
2022-11-18 11:55:01 -0300

    .-.

---
+++ Ex.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+
+App.Run();
+public class BluerControl
+{   
+    public void ApplyBlur(byte[] data)
+    {
+        byte[] exit = new byte[data.Length];
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (((data.Length - 20) > i) && (i-20 > 0))
+            {
+                int aux = 0;
+                for (int reverse = -20; reverse < 20 ; reverse++)
+                {
+                    aux += data[i-reverse];
+                }
+                exit[i] = (byte)(aux/42);
+            }
+        }
+        for (int j = 0; j < exit.Length; j++)
+        {
+            data[j] = exit[j];
+        }
+    }
+}
+
